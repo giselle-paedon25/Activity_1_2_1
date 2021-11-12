253600ea3e3dbc51a3ef625bb6ec1c1767992868
giselle-paedon25 <91690159+giselle-paedon25@users.noreply.github.com>
2021-11-12 13:56:05 -0800

    Add files via upload

--- a121_catch_a_turtle_[initials].py
+++ a121_catch_a_turtle_[initials].py
@@ -1,14 +1,23 @@
 # a121_catch_a_turtle.py
 #-----import statements-----
 import turtle as trtl
+
+import random as rand
+
+import leaderboard as lb
+
+#-----game configuration----
+leaderboard_file_name = "a122_leaderboard.txt"
+leader_names_list = []
+leader_scores_list = []
+player_name = input("Please input your name: ")
+
 t = trtl.Turtle()
 
 score_writer = trtl.Turtle()
 
-import random as rand
-
 counter =  trtl.Turtle()
-#-----game configuration----
+
 sizes = [0.5, 1, 2, 3, 4, 5]
 t.shape("turtle")
 t.shapesize(1.5)
@@ -23,7 +32,7 @@ score_writer.goto(190,190)
 score_writer.pendown()
 
 counter.penup()
-counter.goto(-190,-190)
+counter.goto(-190,190)
 counter.pendown()
 
 font_setup = ("Arial", 20, "normal")
@@ -33,18 +42,43 @@ timer_up = False
 
 
 #-----game functions--------
-def countdown():
-  global timer, timer_up
-  counter.clear()
-  if timer <= 0:
-    counter.write("Time's Up", font=font_setup)
-    timer_up = True
+def manage_leaderboard():
+  global leader_scores_list
+  global leader_names_list
+  global score
+  global spot
+
+  # load all the leaderboard records into the lists
+  lb.load_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list)
+
+  # TODO
+  if (len(leader_scores_list) < 5 or score > leader_scores_list[4]):
+    lb.update_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list, player_name, score)
+    lb.draw_leaderboard(leader_names_list, leader_scores_list, True, t, score)
+
   else:
-    counter.write("Timer: " + str(timer), font=font_setup)
-    timer -= 1
-    counter.getscreen().ontimer(countdown, counter_interval)
+    lb.draw_leaderboard(leader_names_list, leader_scores_list, False, t, score)
+
+def countdown():
+	global timer, timer_up
+	counter.clear()
+	if timer <= 0:
+		counter.write("Time's Up", font=font_setup)
+		timer_up = True
+		manage_leaderboard()
+	else:
+		counter.write("Timer: " + str(timer), font=font_setup)
+		timer -= 1
+		counter.getscreen().ontimer(countdown, counter_interval)
+
+colors = ["red", "blue", "purple", "green", "yellow", "pink", "white", "black"]
+
+bgcolor = ["pink", "purple", "green","blue", "white"]
 
 def change_position():
+	t.stamp()
+	t.fillcolor(rand.choice(colors))
+	wn.bgcolor(rand.choice(bgcolor))
 	t.penup()
 	new_xpos = rand.randint(-200,200)
 	new_ypos = rand.randint(-200,200)
@@ -65,10 +99,12 @@ def t_clicked(x,y):
 	else:
 		t.hideturtle()
 
+
 t.onclick(t_clicked)
 #-----events----------------
+# manages the leaderboard for top 5 scorers
+
 
 wn = trtl.Screen()
-wn.bgcolor("purple")
 wn.ontimer(countdown, counter_interval) 
 wn.mainloop()
